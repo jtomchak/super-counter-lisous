f088d120a8d57b777a3bfb83d1ed06c163b7c157
Jesse Tomchak <you@example.com>
2017-08-28 15:55:45 -0700

    init, with first todo's

---
+++ README.md
@@ -0,0 +1,5 @@
+## super-counter-lisous
+
+1. Create Me
+2. CounterContainer will take a prop of initial value
+3. Refactor CounterContainer into a presentational component. 
---
+++ src/CounterContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class CounterContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  onIncrement() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  onDecrement() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          className="counter__btn-increment"
+          onClick={() => this.onIncrement()}
+        >
+          +
+        </button>
+        <span className="counter__value">
+          {this.state.count}
+        </span>
+        <button
+          className="counter__btn-decrement"
+          onClick={() => this.onDecrement()}
+        >
+          -
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CounterContainer;
